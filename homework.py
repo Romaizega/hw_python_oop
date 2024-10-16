16695de940c7bf5d2eb5ac12ab029c801441a685
Roman Izegov <146068767+Romaizega@users.noreply.github.com>
2024-10-16 17:19:13 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class InfoMessage:
-    """Информационное сообщение о тренировке."""
+    """Training Information Notice."""
 
     training_type: str
     duration: float
@@ -12,7 +12,7 @@ class InfoMessage:
     calories: float
 
     def get_message(self) -> str:
-        """Возвращает сообщение о тренировке."""
+        """Returns a message about the training."""
         return (f'Тип тренировки: {self.training_type}; '
                 f'Длительность: {self.duration:.3f} ч.; '
                 f'Дистанция: {self.distance:.3f} км; '
@@ -21,7 +21,7 @@ class InfoMessage:
 
 
 class Training:
-    """Базовый класс тренировки."""
+    """Base class for training."""
 
     LEN_STEP = 0.65
     M_IN_KM = 1000
@@ -37,20 +37,20 @@ class Training:
         self.weight = weight
 
     def get_distance(self) -> float:
-        """Получить дистанцию в км."""
+        """To get the distance in km."""
         return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
+        """Get average speed of movement."""
         return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
+        """Get the number of calories burned."""
         raise NotImplementedError('Данный метод реализуется '
                                   'отделно для каждого дочернего класса')
 
     def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
+        """Return an informational message about the completed training."""
         return InfoMessage(
             training_type=self.__class__.__name__,
             duration=self.duration,
@@ -61,13 +61,13 @@ class Training:
 
 
 class Running(Training):
-    """Тренировка: бег."""
+    """Training: running."""
 
     CALORIES_MEAN_SPEED_MULTIPLIER: float = 18.0
     CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
+        """Get the amount of calories burned."""
         return (
             self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed()
             + self.CALORIES_MEAN_SPEED_SHIFT
@@ -77,7 +77,7 @@ class Running(Training):
 
 
 class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
+    """Training: power walking."""
 
     MULTIPLIER_1_OF_WEIGHT: float = 0.035
     MULTIPLIER_2_OF_WEIGHT: float = 0.029
@@ -94,7 +94,7 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
+        """Get the number of calories burned."""
         speed_into_ms = self.get_mean_speed() * self.KMH_TO_MSEC
         height_into_m = self.height / self.M_TO_CM
         duration_into_min = self.duration * self.HOUR_TO_MIN
@@ -105,7 +105,7 @@ class SportsWalking(Training):
 
 
 class Swimming(Training):
-    """Тренировка: плавание."""
+    """Training: swimming."""
     LEN_STEP: float = 1.38
     CALORIES_MEAN_SPEED_SHIFT_SWM: float = 1.1
     CALORIES_MEAN_SPEED_MULTIPLIER_SWM: float = 2
@@ -143,7 +143,7 @@ TRAIN_TYPES: dict[str, type[Training]] = {
 
 
 def read_package(workout_type: str, data: list) -> Training:
-    """Прочитать данные полученные от датчиков."""
+    """Read data received from sensors."""
 
     if workout_type not in TRAIN_TYPES:
         raise ValueError(f'{workout_type}: неизвестный тип тренировки')
@@ -151,7 +151,7 @@ def read_package(workout_type: str, data: list) -> Training:
 
 
 def main(training: Training) -> None:
-    """Главная функция."""
+    """Main function."""
     info = training.show_training_info()
     print(info.get_message())
 
